d0cf99286b32a0d41e0e44cbe58c8df601733341
Yael Schärer <yschaere@hsr.ch>
2020-04-22 11:24:46 +0200

    clean up controller with Diagram changes

--- src/main/java/ch/LFG/controller/GroupController.java
+++ src/main/java/ch/LFG/controller/GroupController.java
@@ -1,64 +1,86 @@
 package ch.LFG.controller;
 
 import ch.LFG.entity.Appuser;
-import ch.LFG.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/Group")
 public class GroupController {
-//    @Autowired
-//    private GroupService groupService;
-//    @Autowired
-//    private SessionService sessionService;
-//
-//    //further commands see https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Annotations#OpenAPIDefinition
-//    @Operation(summary = "Get Groups", description = "Get list of all groups")
-//    @GetMapping
-//    public List<Appgroup> findAll() {
-//        return groupService.getAll();
-//    }
-//
-//    @Operation(summary = "Get Group by id", description = "Get one Group by ID")
-//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-//    public Appgroup getGroupProfile(@PathVariable int id){
-//        return groupService.getGroupProfil(id);
-//    }
-//
-//    @Operation(summary = "Create new Group", description = "Create new Group")
-//    @RequestMapping(value="/set", method = RequestMethod.PUT)
-//    public void setGroupProfil(@RequestBody Appgroup group) {
-//        groupService.setUserProfil(group);
-//    }
-//
-//    @Operation(summary = "Update Group", description = "Update existing Group")
-//    @RequestMapping(value="/update", method = RequestMethod.PATCH)
-//    public Appgroup updateGroupProfil(@RequestBody Appgroup group) {
-//        Appgroup updatedGroup = groupService.updateUserProfile(group, group.getGroupId());
-//        return updatedGroup;
-//    }
-//
-//    @Operation(summary = "Update Group", description = "Update existing Group")
-//    @RequestMapping(value="/delete", method = RequestMethod.DELETE)
-//    public Appgroup deleteGroupProfil(@RequestBody Appgroup group) {
-//        Appgroup updatedUser = groupService.updateGroupProfile(group, group.getGroupId());
-//        return updatedUser;
-//    }
-//
-//
-//    @Operation(summary = "Get possible Matches for Group", description = "Get all the possible Matches of a Group by his groupId")
-//    @RequestMapping(value="/PossibleMatches/{id}", method = RequestMethod.GET)
-//    public List<Appuser> getPossibleMatches(@PathVariable Long id, @RequestBody Appgroup group) {
-//        return userService.getPossibleMatches(usgrouper);
-//    }
-//
-//    @Operation(summary = "Get Matches of Group", description = "Get all the Matches of a Group by his groupId")
-//    @RequestMapping(value="/Matches/{id}", method = RequestMethod.GET)
-//    public List<Appuser> getMatches(@PathVariable Long id, @RequestBody Appgroup group) {
-//        return userService.getMatches(group);
-//    }
+
+    @Autowired
+    private GroupService groupService;
+
+
+    //further commands see https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Annotations#OpenAPIDefinition
+
+    @Operation(summary = "Get groups", description = "Get list of groups")
+    @GetMapping
+    public List<Appgroup> findAll() {
+        //return (List<Appgroup>) groupService.getAll();
+        return null;
+    }
+
+
+    @Operation(summary = "Get group by id", description = "Get one group by his ID")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CompletableFuture<Appgroup> getGroupProfil(@PathVariable long groupId){
+        //return groupService.getGroupProfil(groupId);
+        return null;
+    }
+
+    @Operation(summary = "Create new group", description = "Create new group")
+    @RequestMapping( method = RequestMethod.POST)
+    public CompletableFuture<Appgroup> setGroupProfil(@RequestBody Appgroup group) {
+        //return groupService.setGroupProfil(group);
+        return null;
+    }
+
+    @Operation(summary = "Change group", description = "Change group")
+    @RequestMapping( method = RequestMethod.PATCH)
+    public CompletableFuture<Appgroup> updateGroupProfil(@RequestBody Appgroup group) {
+        //groupService.updateGroupProfil(group);
+        return null;
+    }
+
+    @Operation(summary = "Delete group", description = "Delete group")
+    @RequestMapping( value="/{id}", method = RequestMethod.DELETE)
+    public CompletableFuture<Boolean> deleteGroupProfil(@PathVariable Long groupId) {
+        //groupService.updateGroupProfil(group);
+        return null;
+    }
+
+
+    @Operation(summary = "Get Groups from group by groupId", description = "Get all the groups of a group by his groupId")
+    @RequestMapping(value="/MyGroups/{id}", method = RequestMethod.GET)
+    public CompletableFuture<List<Appuser>> getMyGroups(@PathVariable Long groupId) {
+        //return groupService.getMyGroups(groupId);
+        return null;
+    }
+
+    @Operation(summary = "Get Match suggestions for group", description = "Get Match Suggestions of a group by his group")
+    @RequestMapping(value="/Suggestions/{id}", method = RequestMethod.GET)
+    public CompletableFuture<List<Appuser>> getMatchSuggestions(@PathVariable Long groupId) {
+        //return groupService.getMatchSuggestions(groupId);
+        return null;
+    }
+
+    @Operation(summary = "Get Matches of group", description = "Get the Matches of a group by his groupId, where group and Group swiped yes")
+    @RequestMapping(value="/Matches/{id}", method = RequestMethod.GET)
+    public CompletableFuture<List<Appuser>> getMyCurrentMatches(@PathVariable Long groupId) {
+        //return groupService.getMyCurrentMatches(groupId);
+        return null;
+    }
+
+    @Operation(summary = "Match Answer", description = "Give the answer for a proposed match")
+    @RequestMapping(value="/MatchesAnswer/{id}", method = RequestMethod.POST)
+    public CompletableFuture<List<Appuser>> matchAnswer(@RequestBody Long userId, Long groupId, Boolean answer ) {
+        //return groupService.matchAnswer(userId, groupId, answer);
+        return null;
+    }
+
 }
--- src/main/java/ch/LFG/controller/UserController.java
+++ src/main/java/ch/LFG/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 
 @RestController
@@ -15,72 +16,68 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-//    @Autowired
-//    private SessionService sessionService;
-
-    private boolean b;
-
-//    @Operation(summary = "Create new Login", description = "Create new Login")
-//    @RequestMapping(value="/register", method = RequestMethod.PUT)
-//    public void registerUser(@RequestBody UserLogin user) {
-//        sessionService.registerUser(user);
-//    }
 
     //further commands see https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Annotations#OpenAPIDefinition
-    @Operation(summary = "Get Users", description = "Get list of all users")
+
+    @Operation(summary = "Get users", description = "Get list of users")
     @GetMapping
     public List<Appuser> findAll() {
-        return userService.getAll();
+        return (List<Appuser>) userService.getAll();
     }
 
-    @Operation(summary = "Get Users by id", description = "Get one user by his ID")
+    @Operation(summary = "Create new Login", description = "Create new Login")
+    @RequestMapping(value="/Register", method = RequestMethod.PUT)
+    public CompletableFuture<Long> registerUser(@RequestBody Userlogin user) {
+        //return sessionService.registerUser(user);
+        return null;
+    }
+
+    @Operation(summary = "Get User by id", description = "Get one user by his ID")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Appuser getUserProfile(@PathVariable int id){
+    public CompletableFuture<Appuser> getUserProfil(@PathVariable long id){
         return userService.getUserProfil(id);
     }
 
     @Operation(summary = "Create new User", description = "Create new User")
-    @RequestMapping(value="/set", method = RequestMethod.PUT)
-    public void setUserProfil(@RequestBody Appuser user) {
-        userService.setUserProfil(user);
+    @RequestMapping( method = RequestMethod.POST)
+    public CompletableFuture<Appuser> setUserProfil(@RequestBody Appuser user) {
+        //return userService.setUserProfil(user);
+        return null;
     }
 
-    @Operation(summary = "Update User", description = "Update existing user")
-    @RequestMapping(value="/update", method = RequestMethod.PATCH)
-    public Appuser updateUserProfil(@RequestBody Appuser user) {
-        Appuser updatedUser = userService.updateUserProfile(user, user.getUserId());
-        return updatedUser;
+    @Operation(summary = "Change User", description = "Change User")
+    @RequestMapping( method = RequestMethod.PATCH)
+    public CompletableFuture<Appuser> updateUserProfil(@RequestBody Appuser user) {
+        //userService.updateUserProfil(user);
+        return null;
     }
 
-
-//    @Operation(summary = "Get Groups from User by userId", description = "Get all the groups of a user by his userId")
-//    @RequestMapping(value="/Groups/{id}", method = RequestMethod.GET)
-//    public List<Group> getAllGroups(@PathVariable Long id, @RequestBody Appuser user) {
-//        return userService.getAllGroups(user);
-//    }
-//
-//    @Operation(summary = "Get possibleMatches for User", description = "Get all the possible Matches of a User by his userId")
-//    @RequestMapping(value="/PossibleMatches/{id}", method = RequestMethod.GET)
-//    public List<Group> getPossibleMatches(@PathVariable Long id, @RequestBody Appuser user) {
-//        return userService.getPossibleMatches(user);
-//    }
-//
-//    @Operation(summary = "Get Matches of User", description = "Get all the Matches of a User by his userId")
-//    @RequestMapping(value="/Matches/{id}", method = RequestMethod.GET)
-//    public List<Group> getMatches(@PathVariable Long id, @RequestBody Appuser user) {
-//        return userService.getMatches(user);
-//    }
-
-    @Operation(summary = "Temporary test Implementation")
-    @RequestMapping(value="/bool", method = RequestMethod.PUT)
-    public void setBool(@RequestBody Boolean b) {
-        this.b = b;
+    @Operation(summary = "Get Groups from User by userId", description = "Get all the groups of a user by his userId")
+    @RequestMapping(value="/MyGroups/{id}", method = RequestMethod.GET)
+    public CompletableFuture<List<Appgroup>> getMyGroups(@PathVariable Long id) {
+        //return userService.getMyGroups(user);
+        return null;
     }
 
-    @Operation(summary = "Temporary test Implementation")
-    @RequestMapping(value="/bool", method = RequestMethod.GET)
-    public boolean getBool() {
-        return this.b;
+    @Operation(summary = "Get Match suggestions for User", description = "Get Match Suggestions of a User by his userId")
+    @RequestMapping(value="/Suggestions/{id}", method = RequestMethod.GET)
+    public CompletableFuture<List<Appgroup>> getMatchSuggestions(@PathVariable Long userId) {
+        //return userService.getMatchSuggestions(userId);
+        return null;
+    }
+
+    @Operation(summary = "Get Matches of User", description = "Get the Matches of a User by his userId, where User and Group swiped yes")
+    @RequestMapping(value="/Matches/{id}", method = RequestMethod.GET)
+    public CompletableFuture<List<Appgroup>> getMyCurrentMatches(@PathVariable Long userId) {
+        //return userService.getMyCurrentMatches(userId);
+        return null;
+    }
+
+    @Operation(summary = "Match Answer", description = "Give the answer for a proposed match")
+    @RequestMapping(value="/MatchesAnswer/{id}", method = RequestMethod.POST)
+    public CompletableFuture<List<Appgroup>> matchAnswer(@RequestBody Long userId, Long groupId, Boolean answer ) {
+        //return userService.matchAnswer(userId, groupId, answer);
+        return null;
     }
 
 }
